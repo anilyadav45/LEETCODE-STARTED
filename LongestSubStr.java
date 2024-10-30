2caaccbf180573d84d7388840283cc40a8a48c2a
Anil Yadav <rebelanil885@gmail.com>
2024-10-30 22:22:23 +0530

    longestpallindrome

--- LongestSubStr.java
+++ LongestSubStr.java
@@ -1,26 +1,36 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int low = 0;
-        int high = s.length() - 1;
-        String newStr = "";
-        while (high >= low) {
-            newStr = s.substring(low, high);
-            if (newStr.charAt(low) != newStr.charAt(high)) {
-                break;
-            } else {
-                // low++;
-                high--;
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);     // Odd length palindrome
+            int len2 = expandAroundCenter(s, i, i + 1); // Even length palindrome
+            int len = Math.max(len1, len2);
+            
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
             }
         }
-        return newStr;
+        return s.substring(start, end + 1);
+    }
+    
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
     }
 }
 
 public class LongestSubStr {
     public static void main(String[] args) {
-        String s = "babad"; // like here bab and aba is longest palld.
+        String s = "babad";
         Solution obj = new Solution();
-        obj.longestPalindrome(s);
+        System.out.println("Longest Palindromic Substring " + obj.longestPalindrome(s));
     }
-
 }
