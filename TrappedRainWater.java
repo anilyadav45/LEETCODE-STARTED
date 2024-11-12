327f7f7db399b2f5dea70bb58b62ca3293b04fe4
Anil Yadav <rebelanil885@gmail.com>
2024-11-12 23:26:10 +0530

    brute force soln for trapwater

--- TrappedRainWater.java
+++ TrappedRainWater.java
@@ -7,16 +7,14 @@ class Solution {
         int prefixMax[] = new int[height.length];
         prefixMax[0] = height[0];
         for (int i = 1; i < height.length; i++) {
-            prefixMax[i] = Math.max(prefixMax[i - 1], height[i]); // fill the prefixMax arr from from left -
-                                                                  // right smaller to greater
+            prefixMax[i] = Math.max(prefixMax[i - 1], height[i]); // fill the prefixMax arr from left - right store max one only
         }
         // to find suffixMax for rightMax
         int n = height.length;
         int suffixMax[] = new int[height.length];
         suffixMax[n - 1] = height[n - 1]; // last element
         for (int i = n - 2; i >= 0; i--) {
-            suffixMax[i] = Math.max(suffixMax[i + 1], height[i]); // fill the suffixMax arr specific greater to smaller
-                                                                  // value
+            suffixMax[i] = Math.max(suffixMax[i + 1], height[i]); // fill the suffixMax arr from right - left store all greater value only
         }
         // this is how prefix and suffix arr look and compare with the height arr
         // 0 1 1 2 2 2 2 3 3 3 3 3
