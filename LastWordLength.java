4c7557213b4e6ae36ba85338038f46a556d548e5
Anil Yadav <rebelanil885@gmail.com>
2024-11-22 23:26:09 +0530

    last word in given string

--- LastWordLength.java
+++ LastWordLength.java
@@ -1,5 +1,5 @@
 class LastWordLength {
-    public int lengthOfLastWord(String s) {
+    public static int lengthOfLastWord(String s) {
         String newstr = ""; // Initialize the variable to avoid scope issues
         for (int i = s.length() - 1; i >= 0; i--) {
             if (s.charAt(i) == ' ') { 
@@ -11,5 +11,9 @@ class LastWordLength {
             }
         }
         return newstr.length();
+    }
+    public static void main(String[] args) {
+        String s = "I am begginer Software Engineer";
+        System.out.println("The length of last word is : "+ lengthOfLastWord(s));
     }
 }
