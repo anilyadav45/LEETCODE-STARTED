765136a0ca3be1b06f8824a93b4fead348dc5045
Anil Yadav <rebelanil885@gmail.com>
2024-11-27 00:22:04 +0530

    search in matrix - 2

--- SearchMatrix2.java
+++ SearchMatrix2.java
@@ -1,6 +1,6 @@
 public class SearchMatrix2 {
     public static boolean searchMatrix(int[][] matrix, int target) {
-        for (int i = 0; i < matrix[0].length; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             if (isFound(matrix[i], target)) {
                 return true;
             }
@@ -10,8 +10,8 @@ public class SearchMatrix2 {
 
     public static boolean isFound(int arr[], int target) {
         int low = 0;
-        int high = arr.length;
-        while (low < high) {
+        int high = arr.length - 1;
+        while (low <= high) {
             int mid = low + (high - low) / 2;
             if (arr[mid] == target) {
                 return true;
@@ -25,7 +25,10 @@ public class SearchMatrix2 {
     }
 
     public static void main(String[] args) {
-        int matrix[][] = { { 1, 2, 3 }, { 5, 13, 14 }, { 4, 11, 19 }, { 6, 8, 10 } };
+        int matrix[][] = { { 1, 2, 3 },
+                           { 5, 13, 14 },
+                           { 4, 11, 19 },
+                           { 6, 8, 10 } };
         int target = 5;
         System.out.println(searchMatrix(matrix, target));
 
